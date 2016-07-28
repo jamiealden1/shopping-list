99f548b0138927aa4d5fa84375ea1330dd66fab4
Jamie <jamiealden1@gmail.com>
2016-07-28 16:12:30 -0600

    Revision to list item creation

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ $(document).ready(function() {
 
 	$('form#js-shopping-list-form').submit(function(event) {
 		event.preventDefault();
-		$('ul').append('<li><span class="shopping-item">apples</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+		$('ul').append(
+			'<li>' + 
+				'<span class="shopping-item">' + 'text' +'</span>' +
+				'<div class="shopping-item-controls">' + 
+					'<button class="shopping-item-toggle">' + '<span class="button-label">check</span>' + '</button>' + 
+					'<button class="shopping-item-delete">' + '<span class="button-label">delete</span>' + '</button>' +
+				'</div>' + 
+			'</li>');
 	});
 });
