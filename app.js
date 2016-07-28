4e5e4dcf83948d12b16cc9f29c07ce4f653ffa79
Jamie <jamiealden1@gmail.com>
2016-07-28 14:55:58 -0600

    Add List Item

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ $(document).ready(function() {
 	.on('click', 'button.shopping-item-toggle', function(event) {
 		$(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
 	});
+
+	$('form#js-shopping-list-form').submit(function(event) {
+		event.preventDefault();
+		$('ul').append('<li><span class="shopping-item">apples</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+	});
 });
