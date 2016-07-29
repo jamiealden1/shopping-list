95fe64647c35b62e938b9937eeae772051512d98
Jamie Alden <jamielynn_21@Macintosh.local>
2016-07-28 19:15:25 -0600

    Complete project

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
+	//delete list item
 	$('ul.shopping-list').on('click', 'button.shopping-item-delete', function(event) {
 		this.closest('li').remove();
 	})
+	//check/uncheck item
 	.on('click', 'button.shopping-item-toggle', function(event) {
 		$(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
 	});
-
+	//add new list item with text from form input
 	$('form#js-shopping-list-form').submit(function(event) {
 		event.preventDefault();
 		$('ul').append(
 			'<li>' + 
-				'<span class="shopping-item">' + 'text' +'</span>' +
+				'<span class="shopping-item">' + $('#shopping-list-entry').val() +'</span>' +
 				'<div class="shopping-item-controls">' + 
 					'<button class="shopping-item-toggle">' + '<span class="button-label">check</span>' + '</button>' + 
 					'<button class="shopping-item-delete">' + '<span class="button-label">delete</span>' + '</button>' +
 				'</div>' + 
 			'</li>');
+		//clear form input
+		$(this)[0].reset();
 	});
 });
